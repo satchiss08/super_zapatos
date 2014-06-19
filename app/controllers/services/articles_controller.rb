8f8e3afad426adb3b479891befffc4caa337b0f2
satchiss08 <satchiss08@gmail.com>
2014-06-18 21:41:42 -0600

    Add store_name, success and total elements on json

--- app/controllers/services/articles_controller.rb
+++ app/controllers/services/articles_controller.rb
@@ -3,12 +3,17 @@ include ActionView::Helpers::TextHelper
 class Services::ArticlesController < ApplicationController
   def index
     @articles = Article.all
+
+    @articles_list = @articles.map do |a|
+      { :id => a.id, :name => a.name, :description => a.description, :price => a.price, :total_in_shelf => a.total_in_shelf, :total_in_vault => a.total_in_vault, :store_name => a.store.name }
+    end
+    
     json_articles = Hash.new()
 
     if @articles.count > 1
-      json_articles["articles"] = @articles
+      json_articles["articles"] = @articles_list
     else 
-      json_articles["article"] = @articles
+      json_articles["article"] = @articles_list
     end
 
     json_articles["success"] = true
--- app/controllers/services/stores_controller.rb
+++ app/controllers/services/stores_controller.rb
@@ -1,12 +1,17 @@
 class Services::StoresController < ApplicationController
   def index
     @stores = Store.all
+
+    @stores_list = @stores.map do |s|
+      { :id => s.id, :name => s.name, :address => s.address }
+    end
+
     json_stores = Hash.new()
 
     if @stores.count > 1
-      json_stores["stores"] = @stores
+      json_stores["stores"] = @stores_list
     else 
-      json_stores["store"] = @stores
+      json_stores["store"] = @stores_list
     end
     
     json_stores["success"] = true
