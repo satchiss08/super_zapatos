d65d420a170d06a9bcb6fe21bab3959201e575fc
satchiss08 <satchiss08@gmail.com>
2014-06-19 10:07:35 -0600

    Add error messages on articles by store

--- app/controllers/services/articles_controller.rb
+++ app/controllers/services/articles_controller.rb
@@ -8,9 +8,18 @@ class Services::ArticlesController < ApplicationController
   end
 
   def show
-    @articles = Article.where(:store_id => params[:id])
-    articles_parser
-    json_response
+    if numeric?(params[:id])
+      @articles = Article.where(:store_id => params[:id])
+      if !@articles.empty?
+        @articles = Article.where(:store_id => params[:id])
+        articles_parser
+        json_response
+      else
+        error_message(404)
+      end
+    else
+      error_message(400)
+    end
   end
 
   private
@@ -40,5 +49,28 @@ class Services::ArticlesController < ApplicationController
 
       @json_articles["success"] = true
       @json_articles["total_elements"] = @articles.count
+    end
+
+    def error_message(error)
+      @json_articles = Hash.new()
+
+      if error == 400
+        @json_articles["error_msg"] = "Bad Request"
+        @json_articles["error_code"] = 400
+      else
+        @json_articles["error_msg"] = "Record not Found"
+        @json_articles["error_code"] = 404
+      end
+
+      @json_articles["success"] = false
+
+      respond_to do |format|
+        format.html
+        format.json { render :json => @json_articles }
+      end
+    end
+
+    def numeric?(object)
+      true if Float(object) rescue false
     end
 end
