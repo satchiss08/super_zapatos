4f8a7b616e2d49ee3e41f9ce1bd85558f19edd5f
satchiss08 <satchiss08@gmail.com>
2014-06-18 18:54:23 -0600

    Add parameters for json

--- app/controllers/services/articles_controller.rb
+++ app/controllers/services/articles_controller.rb
@@ -1,8 +1,22 @@
+include ActionView::Helpers::TextHelper
+
 class Services::ArticlesController < ApplicationController
   def index
     @articles = Article.all
+    json_articles = Hash.new()
+
+    if @articles.count > 1
+      json_articles["articles"] = @articles
+    else 
+      json_articles["article"] = @articles
+    end
+
+    json_articles["success"] = true
+    json_articles["total_elements"] = @articles.count
+    
     respond_to do |format|
-      format.json { render :json => @articles }
+      format.html
+      format.json { render :json => json_articles }
     end
   end
 end
--- app/controllers/services/stores_controller.rb
+++ app/controllers/services/stores_controller.rb
@@ -1,8 +1,20 @@
 class Services::StoresController < ApplicationController
   def index
     @stores = Store.all
+    json_stores = Hash.new()
+
+    if @stores.count > 1
+      json_stores["stores"] = @stores
+    else 
+      json_stores["store"] = @stores
+    end
+    
+    json_stores["success"] = true
+    json_stores["total_elements"] = @stores.count
+    
     respond_to do |format|
-      format.json { render :json => @stores }
+      format.html
+      format.json { render :json => json_stores }
     end
   end
 end
